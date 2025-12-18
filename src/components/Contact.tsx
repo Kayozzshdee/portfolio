9805ea6ffa977613d721d3d6114ba6d75eba0909
Kayozzshdee <kayozzbenn.816@gmail.com>
2025-12-18 14:17:52 +0800

    repair

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,9 +4,7 @@ import { FaFacebook } from "react-icons/fa";
 
 
 export default function Contact(){
-
-
-
+    
     return(
     <div id="contact" className="bg-[#0e141e] w-full flex flex-col gap-4 py-20">
         <h1 className="text-white text-2xl flex justify-center ">Contact me</h1>
